41dff5e5afc7ef4d6d857eedd93b73d46bcdde07
AleksandrIB <cucheryavyh.sasha@yandex.ru>
2020-09-28 14:49:51 +0300

    Commit 1

---
+++ src/main/java/org/cucheryavyh/Start/HelloController.java
@@ -0,0 +1,13 @@
+package org.cucheryavyh.Start;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String home()
+    {
+        return "home";
+    }
+}
